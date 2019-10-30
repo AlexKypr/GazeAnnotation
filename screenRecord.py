37db9f1b423ac87647ac5d8649ed79f070231dbd
Alexandros-Charalampos Kyprianidis <alexkypr.ece@gmail.com>
2019-10-30 15:20:52 +0200

    Saving Monitor & Printing Mouse activity
    Working Version

--- screenRecord.py
+++ screenRecord.py
@@ -1,65 +1,89 @@
-
 import time
 import cv2
 import mss
 import numpy
+from pynput import mouse, keyboard
+from threading import Thread
+from queue import Queue
 
-frame_width = 1280
-frame_height = 720
-frameRate = 30.0
-outputPath = "screen.avi"
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter(outputPath, fourcc, frameRate,
-                      (frame_width, frame_height))
-
-
-
-with mss.mss() as sct:
-    # Capture whole screen
-    monitor = sct.monitors[1]
-
-    while 'Screen capturing':
-        last_time = time.time()
-
-        # Get raw pixels from the screen, save it to a Numpy array
-        img = numpy.array(sct.grab(monitor))
-        img = cv2.resize(img, (frame_width, frame_height))
-        frame = img
-        frame = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        cv2.putText(frame, "FPS: %f" % (1.0 / (time.time() - last_time)),
-                    (10, 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-        out.write(frame)
-        cv2.imshow('frame', frame)
-
-        # Display the picture
-        #cv2.imshow('OpenCV/Numpy normal', img)
-
-        # Display the picture in grayscale
-        # cv2.imshow('OpenCV/Numpy grayscale',
-        # cv2.cvtColor(img, cv2.COLOR_BGRA2GRAY))
-
-        #print('fps: {0}'.format(1 / (time.time()-last_time)))
-
-
-
-        # Press "q" to quit
-        if cv2.waitKey(25) & 0xFF == ord('q'):
-            cv2.destroyAllWindows()
-            break
 '''
+Used to save the monitored screen
+'''
+def screen_record(in_q):
+    frame_width = 1280
+    frame_height = 720
+    frameRate = 25.0
+    #i = 0
+    outputPath = "screen.avi"
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter(outputPath, fourcc, frameRate, (frame_width, frame_height))
 
-from mss import mss
-from PIL import Image
-
-def capture_screenshot():
-    # Capture entire screen
-    with mss() as sct:
+    with mss.mss() as sct:
+        # Capture whole screen
         monitor = sct.monitors[1]
-        sct_img = sct.grab(monitor)
-        # Convert to PIL/Pillow Image
-        return Image.frombytes('RGB', sct_img.size, sct_img.bgra, 'raw', 'BGRX')
+        while "Screen capturing":
+            #i +=1
+            #print("Iteration: %d" % i)
+            #last_time = time.time()
+            # Get raw pixels from the screen, save it to a Numpy array
+            img = numpy.array(sct.grab(monitor))
+            img = cv2.resize(img, (frame_width, frame_height))
+            frame = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            out.write(frame)
+            if in_q.get() == _sentinel:
+                break
+             #   break
+            #cv2.imshow('frame', frame)
+            #cv2.putText(frame, "FPS: %f" % (1.0 / (time.time() - last_time)),
+                    # (10, 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+            #print("FPS: %f" % (1.0 / (time.time() - last_time)))
+            # Press "q" to quit
+            #k = cv2.waitKey(0)
+            #print("eftase edw")
+            #print(cv2.waitKey(0))
+            #if cv2.waitKey(25) & 0xFF == ord("q"):
+            #    cv2.destroyAllWindows()
+            #    break
+            #if cv2.waitKey(0) == 27:
+               # print("MPHKEE")
+               #cv2.destroyAllWindows()
+               #break
 
-img = capture_screenshot()
-img.show()
 '''
+Used to monitor the movement, click and scrolling behavior of mouse
+'''
+def listen(out_q):
+    def on_move(x, y):
+        print(x, y)
+
+    def on_click(x, y, button, pressed):
+        print(x, y, button, pressed)
+
+    def on_scroll(x, y, dx, dy):
+        print(x, y, dx, dy)
+
+    def on_press(key):
+        if key == keyboard.Key.esc:
+            # Collect events until pressed Esc key
+            out_q.put(_sentinel)
+            mouse_listener.stop()
+            key_listener.stop()
+
+    mouse_listener = mouse.Listener(on_move = on_move, on_click=on_click, on_scroll=on_scroll)
+    mouse_listener.start()
+    with keyboard.Listener(on_press=on_press) as key_listener:
+        key_listener.join()
+
+
+
+if __name__ == '__main__':
+    _sentinel = object()
+    q = Queue()
+    t1 = Thread(target = screen_record, args =(q, ))
+    t2 = Thread(target = listen, args =(q, ))
+    t1.start()
+    t2.start()
+
+    q.join()
+
